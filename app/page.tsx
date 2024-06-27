be2e871b1c42680cb5e328359e270ca6762dd8f5
Atul Tingre <atultingre.work@gmail.com>
2024-06-27 14:03:47 +0530

    shadcn ui installed

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
-  return (
-  <div>Atul TIngre</div>
-  );
+  return <Button>Atul TIngre</Button>;
 }
